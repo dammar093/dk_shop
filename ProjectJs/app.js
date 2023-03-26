a4f838fe91096e70f3509a38731e289a88b720c9
Dammar Singh Rana <119866651+dammar093@users.noreply.github.com>
2023-03-26 09:54:35 +0545

    Add files via upload

---
+++ ProjectJs/app.js
@@ -0,0 +1,12 @@
+
+function myFunction() {
+    let menu = document.querySelector('menu-bar');
+    let navbar = document.querySelector('.navbar');
+    if (navbar.style.display == 'none') {
+        navbar.style.display = 'block';
+    }
+    else {
+        navbar.style.display = 'none';
+    }
+}
+
